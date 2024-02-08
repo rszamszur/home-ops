7f1307ed6ed8f2d464ae23a09f46134893931248
Radosław Szamszur <github@rsd.sh>
2024-02-08 23:07:35 +0100

    chore: updates shell.nix buildInputs

--- shell.nix
+++ shell.nix
@@ -2,11 +2,18 @@
 
 pkgs.mkShell {
   buildInputs = [
+    pkgs.gnumake
     pkgs.minikube
     pkgs.kubernetes-helm
     pkgs.jq
     pkgs.kubectl
     pkgs.yamllint
     pkgs.kubeconform
+    pkgs.terraform
+    pkgs.packer
+    pkgs.talosctl
+    pkgs.yq
+    pkgs.age
+    pkgs.sops
   ];
 }
